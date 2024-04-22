d246074d67a82878031999c0afd1ffb9b7bf7ee6
CAMILO ACACIO <C.ACACIO@Bless.com>
2024-04-22 17:56:24 -0500

    terminar

---
+++ app/Http/Controllers/OrderProcessDetailController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderProcessDetail;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderProcessDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrderProcessDetail  $orderProcessDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderProcessDetail $orderProcessDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OrderProcessDetail  $orderProcessDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrderProcessDetail $orderProcessDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrderProcessDetail  $orderProcessDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrderProcessDetail $orderProcessDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrderProcessDetail  $orderProcessDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderProcessDetail $orderProcessDetail)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_04_22_175300_create_order_process_detail_request_quantities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_process_detail_request_quantities', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_process_detail_request_quantities');
+    }
+};
